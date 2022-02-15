2bd45622060f9fce14ae61afb84ae538fe3ff75c
Friderika Bilkoova <bfridu@gmail.com>
2022-02-15 11:02:21 +0100

    changing navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 export default function App() {
   return (
     <main>
-      <Navbar />
+      <Navigation />
       <About />
       <Projects />
       <Skills />
